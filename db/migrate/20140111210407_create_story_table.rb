5bece55c1d4bc55405f4452ee0a97ffeb9c297e1
JC <jorge.e.colindres@gmail.com>
2014-01-11 16:13:29 -0500

    ran migrations

---
+++ db/migrate/20140111210407_create_story_table.rb
@@ -0,0 +1,14 @@
+class CreateStoryTable < ActiveRecord::Migration
+  def change
+    create_table(:users) do |t|
+      t.string  :as_a
+      t.string :i_want_to
+      t.string :so_i_can
+      t.integer :user_id
+      t.integer :parent_id
+      t.boolean :completed
+
+      t.timestamps
+    end
+  end
+end
